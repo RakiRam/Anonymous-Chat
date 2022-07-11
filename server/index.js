28f235a31ee754bad2f4acad2a6aefc0c1f5fef7
Ramakrishna Manthena <67856784+RakiRam@users.noreply.github.com>
2022-07-11 19:44:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(process.env.PORT || 8000,{
+const io = require('socket.io')(process.env.PORT || 3000,{
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
