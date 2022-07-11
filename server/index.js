c98f843cbe2c54ebe76d2bfbd09642169abdb830
Ramakrishna Manthena <67856784+RakiRam@users.noreply.github.com>
2022-07-11 17:17:13 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const io = require('socket.io')(8000,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+      }
+})
+const users = {};
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log("New user",name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        console.log(message);
+        socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message => {
+        console.log(message);
+        socket.broadcast.emit('left', users[socket.id])
+        delete users[socket.id];
+    });
+});
